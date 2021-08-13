6f7f430c4d7812b3c76208b7eb94f0392a6067df
nilhan sarwar <77848392+sarwarnilhan@users.noreply.github.com>
2021-08-14 00:06:19 +0600

    Add files via upload

---
+++ conversionrate.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>conversion</title>
+	
+</head>
+<body>
+	<h1 align = "left">Page 2 [Conversion Rate]</h1>
+	
+	
+</article>
+
+<article align = "left">
+	<p>
+		<h2 > Conversion </h2>
+
+		<b>1.Home </b> <a href="home.php" target="blank">Click here</a>
+
+
+		<b>2.Conversion Rate </b><a href="conversionrate.php" target="blank">Click here</a><br>
+
+
+		<b>3.History </b> <a href="history.php" target="blank">Click here</a><br>
+
+
+
+</p>
+</article>
+
+<form><table>
+	<tr>
+	
+					
+				<td> <input type="text" name="value"></td>
+			
+			</tr>
+
+
+					<tr>
+				<td> <input type="Number" name="value"></td>
+			
+			</tr>
+
+			<tr>
+				<td> <input type="Number" name="result"></td>
+			
+			</tr>
+
+			
+			<tr> <td><input type="submit" name="submit">
+			</td></tr>
+
+
+					</table>
+				
+
+</form>
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>conversion</title>
+	
+</head>
+<body>
+	<h1 align = "left">Page 1 [Home page]</h1>
+	
+	
+</article>
+
+<article align = "left">
+	<p>
+		<h2 > Conversion </h2>
+
+		<b>1.Home </b> <a href="home.php" target="blank">Click here</a>
+
+
+		<b>2.Conversion Rate </b><a href="conversionrate.php" target="blank">Click here</a><br>
+
+
+		<b>3.History </b> <a href="history.php" target="blank">Click here</a><br>
+
+
+
+</p>
+</article>
+
+<form><table>
+	<tr>
+				<td> Converter :
+					<select name = "Select"> 
+						<option value "">select</option>
+						<option value="Feet to inch"> Feet to inch</option>
+
+						<option value="inch to feet"> inch to feet</option>
+
+						<option value="metre to centimetre"> metre to centimetre</option>
+
+						<option value="metre to kilometre"> m to km</option>
+
+
+
+					 </select></td>
+					</tr>
+
+
+					<tr><td>Value :</td>
+				<td> <input type="Number" name="value"></td>
+			
+			</tr>
+
+			<tr><td>Result :</td>
+				<td> <input type="Number" name="result"></td>
+			
+			</tr>
+
+			
+			<tr> <td><input type="submit" name="submit">
+			</td></tr>
+
+
+					</table>
+				
+
+</form>
+
+
+
+
+
+
+
+
+</body>
+</html>
